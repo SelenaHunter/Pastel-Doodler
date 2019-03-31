2a6955d23a0b47eff227ea59c1f5b6f660f55f0d
Selena Hunter <selenahunter99@gmail.com>
2019-03-31 03:03:47 -0500

    Add support for white color

--- doodler.js
+++ doodler.js
@@ -45,12 +45,6 @@ canvas.addEventListener('mousedown', (e) => {
 			[lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
-canvas.addEventListener('touchstart', (e) => {
-	isDrawing = true;
-			[lastX, lastY] = [e.offsetX, e.offsetY];
-});
-
-
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
