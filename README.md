e3d74eb6199f7f651080c7349190a809167b707f
Selena Hunter <selenahunter99@gmail.com>
2019-04-02 19:07:45 -0500

    Add mobile support; update README.md

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A modified Canvas tutorial from Wes Bos featuring a color selector and the abili
 Draw with the mouse. The clear button will clear the canvas. The save button will export the file to a .PNG that can be downloaded. The pick color button will open a menu where a new color can be selected. Click the button again to close the menu.
 
 https://selenahunter.github.io/Pastel-Doodler/
+
+## Acknowledgements
+This web app was made with help from tutorials by Wes Bos (https://courses.wesbos.com) and Ben Centra (https://github.com/bencentra/canvas/tree/master/signature).
--- doodler.js
+++ doodler.js
@@ -119,3 +119,51 @@ function changeColor(choice) {
 		lum = '100%';
 	}
 }
+
+
+// Mobile support
+canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
+
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target.nodeValue == "canvas") {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target.nodeValue == "canvas") {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target.nodeValue == "canvas") {
+    e.preventDefault();
+  }
+}, false);
