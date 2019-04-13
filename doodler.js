202a8f1336b298f5b57c101da2d1978e5f311ed5
Selena Hunter <selenahunter99@gmail.com>
2019-04-12 23:30:09 -0500

    Fix scroll [TEST]

--- doodler.js
+++ doodler.js
@@ -153,17 +153,17 @@ function getTouchPos(canvasDom, touchEvent) {
 }
 
 document.body.addEventListener("touchstart", function (e) {
-  if (e.target.nodeValue == "canvas") {
+  if (e.target == "canvas") {
     e.preventDefault();
   }
 }, false);
 document.body.addEventListener("touchend", function (e) {
-  if (e.target.nodeValue == "canvas") {
+  if (e.target == "canvas") {
     e.preventDefault();
   }
 }, false);
 document.body.addEventListener("touchmove", function (e) {
-  if (e.target.nodeValue == "canvas") {
+  if (e.target == "canvas") {
     e.preventDefault();
   }
 }, false);
